584df948b7dc104ab916a1671603dc28b1cc5ab9
metin yildirim <metin_yildirim@outlook.be>
2025-09-23 11:28:56 +0200

    Add project files.

---
+++ WebAppAspLayered/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppAspLayered.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
