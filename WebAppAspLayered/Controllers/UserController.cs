89be132798369367150bcdf03bc15c3d6d7b897b
metin yildirim <metin_yildirim@outlook.be>
2025-09-23 13:20:33 +0200

    create user controller

---
+++ WebAppAspLayered/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WebAppAspLayered.BLL.Services;
+using WebAppAspLayered.DL.Entities;
+using WebAppAspLayered.Mappers;
+using WebAppAspLayered.Models.Users;
+
+namespace WebAppAspLayered.Controllers;
+
+public class UserController : Controller
+{
+    private readonly UserService _userService;
+
+    public UserController(UserService userService)
+    {
+        _userService = userService;
+    }
+
+    public IActionResult Register()
+    {
+        return View(new RegisterFormDto());
+    }
+
+    [HttpPost]
+    public IActionResult Register([FromForm] RegisterFormDto form)
+    {
+        if (!ModelState.IsValid)
+        {
+            form.Password = "";
+            form.ConfirmPassword = "";
+            return View(form);
+        }
+
+        try
+        {
+            _userService.Register(form.ToUser());
+        }
+        catch (Exception ex)
+        {
+            ModelState.AddModelError("Error", ex.Message);
+            form.Password = "";
+            return View(form);
+        }
+
+        return RedirectToAction("Login", "User");
+    }
+
+    public IActionResult Login()
+    {
+        return View(new LoginFormDto());
+    }
+
+    [HttpPost]
+    public IActionResult Login([FromForm] LoginFormDto form)
+    {
+        if (!ModelState.IsValid)
+        {
+            form.Password = "";
+            return View(form);
+        }
+
+        try
+        {
+            User? user = _userService.Login(form.Email, form.Password);
+        }
+        catch (Exception ex)
+        {
+            ModelState.AddModelError("Error", ex.Message);
+            form.Password = "";
+            return View(form);
+        }
+
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ WebAppAspLayered/Mappers/UserMappers.cs
@@ -0,0 +1,16 @@
+﻿using WebAppAspLayered.DL.Entities;
+using WebAppAspLayered.Models.Users;
+
+namespace WebAppAspLayered.Mappers;
+
+public static class UserMappers
+{
+    public static User ToUser(this RegisterFormDto form)
+    {
+        return new User()
+        {
+            Email = form.Email,
+            Password = form.Password,
+        };
+    }
+}
